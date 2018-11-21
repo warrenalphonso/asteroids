c47a6a0daf3f44efe03e713ae6b996489a5da4ac
Warren Alphonso <warrenalphonso02@gmail.com>
2018-11-21 06:47:14 -0800

    added basic matrix fnctionalities

--- neural-network.js
+++ neural-network.js
@@ -12,7 +12,7 @@ class Matrix{
 
     //initialize with zeros if no data provided
     if (data == null || data.length == 0){
-      this.data = [];
+      this._data = [];
       for (let i = 0; i < this._rows; i++){
         this._data[i] = [];
         for (let j = 0; j < this._cols; j++){
@@ -37,5 +37,102 @@ class Matrix{
 
   get data(){
     return this._data;
+  }
+
+  //add two matrices
+  static add(m0, m1){
+    Matrix.checkDimensions(m0, m1);
+    let m = new Matrix(m0.rows, m0.cols);
+    for (let i = 0; i < m.rows; i++){
+      for (let j = 0; j < m.cols; j++){
+        m.data[i][j] =m0.data[i][j] + m1.data[i][j];
+      }
+    }
+    return m;
+  }
+
+  //check matrices have same dimensions
+  static checkDimensions(m0, m1){
+    if (m0.rows != m1.rows || m0.cols != m1.cols){
+      throw new Error('Matrices are of different dimensions')
+    }
+  }
+
+  //convert array to a one rowed matrix
+  static convertFromArray(arr){
+    return new Matrix(1, arr.length, [arr]);
+  }
+
+  //dot product of two matrices
+  static dot(m0, m1){
+    if (m0.cols != m1.rows){
+      throw new Error('Matrices are not dot compatible');
+    }
+    let m = new Matrix(m0.rows, m1.cols);
+    for (let i = 0; i < m.rows; i++){
+      for (let j = 0; j < m.cols; j++){
+        let sum = 0;
+        for (let k = 0; k < m0.cols; k++){
+          sum += m0.data[i][k] * m1.data[k][j];
+        }
+        m.data[i][j] = sum;
+      }
+    }
+    return m
+  }
+
+  //apply a function to each cell of the given matrix
+  static map(m0, mFunction){
+    let m = new Matrix(m0.rows, m0.cols);
+    for (let i = 0; i < m.rows; i++){
+      for (let j = 0; j < m.cols; j++){
+        m.data[i][j] = mFunction(m0.data[i][j]);
+      }
+    }
+    return m;
+  }
+
+  //multiply two matrices; not dot product
+  static multiply(m0, m1){
+    Matrix.checkDimensions(m0, m1);
+    let m = new Matrix(m0.rows, m0.cols);
+    for (let i = 0; i < m.rows; i++){
+      for (let j = 0; j < m.cols; j++){
+        m.data[i][j] =m0.data[i][j] * m1.data[i][j];
+      }
+    }
+    return m;
+  }
+
+  //subtract two matrices
+  static subtract(m0, m1){
+    Matrix.checkDimensions(m0, m1);
+    let m = new Matrix(m0.rows, m0.cols);
+    for (let i = 0; i < m.rows; i++){
+      for (let j = 0; j < m.cols; j++){
+        m.data[i][j] =m0.data[i][j] - m1.data[i][j];
+      }
+    }
+    return m;
+  }
+
+  //find transpose of matrix
+  static transpose(m0){
+    let m = new Matrix(m0.cols, m0.rows);
+    for (let i = 0; i < m0.rows; i++){
+      for (let j = 0; j < m0.cols; j++){
+        m.data[j][i] =m0.data[i][j];
+      }
+    }
+    return m;
+  }
+
+  //apply random weights between -1 and 1
+  randomWeights(){
+    for (let i = 0; i < this.rows; i++){
+      for (let j = 0; j < this.cols; j++){
+        this.data[i][j] = Math.random() * 2 - 1;
+      }
+    }
   }
 }
